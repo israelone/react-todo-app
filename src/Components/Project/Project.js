766b3cc4683a3db6cdc34c0330aa9494a7ca917e
israel <itzisraelone@gmail.com>
2020-11-20 17:00:38 -0600

    update

---
+++ src/Components/Project/Project.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const project = (props) =>{
+    return(
+    <div>
+        <div>
+            <h2>hi</h2>
+            <h4>ddg</h4>
+        </div>
+        <div>
+            <h3>fgdf</h3>
+        </div>
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div>
+        </div>
+    </div>
+    )
+}
+
+export default project;
